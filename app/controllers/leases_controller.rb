6b2e081f925f5a02a342816ee99ad18b230fb558
Laszlo Arnould <laszlo.arnould@gmail.com>
2017-03-13 23:45:35 +0100

    Indexation by region

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -7,31 +7,38 @@ class LeasesController < ApplicationController
 
   def create
     @lease = Lease.new(lease_params)
-
-
-
-
     sign_month = Date::MONTHNAMES[@lease.start_month]
     # get data from json
       file = File.read('app/assets/datas/health_indices.json')
       data_hash = JSON.parse(file)
-      sign_index = data_hash[sign_month + " " + @lease.start_year.to_s]
-      if @lease.region == "Région Wallonne"
-        base_year = @lease.indexation_year - 1
-        base_month = @lease.start_month - 1
-        if base_month == 0
-          changed_base_month = "December"
-          current_date = changed_base_month + " " + base_year
-          current_index = data_hash[current_date]
+      # Define sign index
+        sign_index = data_hash[sign_month + " " + @lease.start_year.to_s]
+      # Define current index on base of the region of the lease
+        if @lease.region == "Région Wallonne"
+          base_year = @lease.indexation_year - 1
+          base_month = @lease.start_month - 1
+          if base_month == 0
+            changed_base_month = "December"
+            changed_base_year = base_year - 1
+            current_date = changed_base_month + " " + changed_base_year.to_s
+            current_index = data_hash[current_date]
+          else
+            changed_base_month = Date::MONTHNAMES[base_month]
+            current_date = changed_base_month + " " + base_year.to_s
+            current_index = data_hash[current_date]
+          end
         else
-          changed_base_month = Date::MONTHNAMES[@lease.start_month - 1]
-          current_date = changed_base_month + " " + base_year
-          current_index = data_hash[current_date]
+          base_month = @lease.start_month - 1
+          if base_month == 0
+            changed_base_month = "December"
+            base_year = @lease.indexation_year - 1
+            current_date = changed_base_month + " " + base_year.to_s
+            current_index = data_hash[current_date]
+          else
+            current_date = Date::MONTHNAMES[base_month] + " " + @lease.indexation_year.to_s
+            current_index = data_hash[current_date]
+          end
         end
-      else
-
-
-      end
     # method for indexation
       indexation = index(@lease.rent, current_index, sign_index)
       @lease.new_rent = indexation
